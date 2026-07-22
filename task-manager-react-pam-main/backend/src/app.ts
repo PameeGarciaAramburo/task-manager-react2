cf8c94eae528760c924f785146e2c737d2cc501c
PameeGarciaAramburo <pamela.ga.pga@gmail.com>
2026-07-22 12:43:55 -0400

    Add Railway CD

--- task-manager-react-pam-main/backend/src/app.ts
+++ task-manager-react-pam-main/backend/src/app.ts
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "ok"
+  });
+});
+
 app.get("/tasks", async (req: Request, res: Response) => {
   const result = await pool.query('SELECT * FROM "Task" ORDER BY id ASC');
   res.json(result.rows);
--- task-manager-react-pam-main/backend/src/index.ts
+++ task-manager-react-pam-main/backend/src/index.ts
@@ -1,49 +1,6 @@
-/*import express, { Request, Response } from "express";
-import cors from "cors";
-import pool from "./db";
-
-const app = express();
-const PORT = 3000;
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/tasks", async (req: Request, res: Response) => {
-  const result = await pool.query("SELECT * FROM tasks ORDER BY id ASC");
-  res.json(result.rows);
-});
-
-app.post("/tasks", async (req: Request, res: Response) => {
-  const { title } = req.body;
-  const result = await pool.query(
-    "INSERT INTO tasks (title, completed) VALUES ($1, false) RETURNING *",
-    [title]
-  );
-  res.json(result.rows[0]);
-});
-
-app.delete("/tasks/:id", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  await pool.query("DELETE FROM tasks WHERE id = $1", [id]);
-  res.json({ message: "Task deleted" });
-});
-
-app.put("/tasks/:id", async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  const result = await pool.query(
-    "UPDATE tasks SET completed = NOT completed WHERE id = $1 RETURNING *",
-    [id]
-  );
-  res.json(result.rows[0]);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});*/
-
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
